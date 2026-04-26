116e77e17c3776392700e1d37e082255b0ba208e
Aditya raj <adityaraj5112005@gmail.com>
2026-04-26 23:28:31 +0530

    add script.js

---
+++ Backend/script.js
@@ -0,0 +1,25 @@
+const File = require('./models/file');
+const fs = require('fs');
+const path = require('path');
+const connectDB = require('./config/db');
+connectDB();
+
+async function fetchData(){
+    //  fetching 24 hr old files and deleting them
+    const pastdate = new Date(Date.now() - 24*60*60*1000);
+    const files = await File.find({createdAt: { $lt: pastdate}});
+    if(files.length){
+        for(const file of files){
+            try{
+                fs.unlinkSync(file.path);
+            await file.remove();
+            console.log(`successfully deleted ${file.filename}`);
+            }catch(err){
+                console.log(`Error while deleting file ${file.filename}`)
+            }
+        }
+        console.log('job done!');
+    }
+}
+
+fetchData().then(process.exit);
